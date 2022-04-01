fa3e17c37cf385cd2c7d004d2c3a1f8762c12720
klarubbio <78495457+klarubbio@users.noreply.github.com>
2022-04-01 19:27:53 -0400

    Create COPYTHISFILE.cpp

---
+++ COPYTHISFILE.cpp
@@ -0,0 +1,12 @@
+// COPYTHISFILE.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "My name is <<YOUR NAME HERE>>";
+}
+
+
